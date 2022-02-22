1e9adec355cc2454b5d9e84c5b8ebf8e6d8eb72f
= <=>
2022-02-22 11:30:22 +0000

    initial commit

---
+++ Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SpecFlowCalculater
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+            // throw new NotImplementedException();
+        }
+
+        public int Subtract()
+        {
+            return FirstNumber - SecondNumber;
+        }
+    }
+}
